25a7f3b5a53998bbac99c4440e4f3efd57249eac
Kevin Wilson <kevin@partcoffee.com>
2023-02-04 16:57:44 +0000

    fix: align app styles

--- src/app.tsx
+++ src/app.tsx
@@ -4,12 +4,18 @@ import { Home } from "./views/home";
 
 const App = () => (
   <div className="app">
-    <div className="app__row">
-      <a href="https://www.thisisbud.com/" target="_blank" rel="noreferrer">
-        <img src="/bankly.svg" className="logo" alt="Bud logo" />
-      </a>
+    <div className="app__header">
+      <div className="container">
+        <div className="app__row">
+          <a href="https://www.thisisbud.com/" target="_blank" rel="noreferrer">
+            <img src="/bankly.svg" className="logo" alt="Bud logo" />
+          </a>
+        </div>
+      </div>
     </div>
-    <Home />
+    <div className="container">
+      <Home />
+    </div>
   </div>
 );
 
